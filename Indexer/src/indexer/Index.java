c6772e46aba308e7fad3fdbc3ea1a306532b4071
tafmi <teo.moutsos@gmail.com>
2016-03-19 07:50:00 +0200

    Update Index.java

--- Indexer/src/indexer/Index.java
+++ Indexer/src/indexer/Index.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package indexer;
 
 import java.io.File;
@@ -32,10 +28,6 @@ import org.apache.lucene.util.BytesRef;
 import org.apache.tika.exception.TikaException;
 import org.xml.sax.SAXException;
 
-/**
- *
- * @author Teo
- */
 public class Index {
 
     private final Directory directory;
